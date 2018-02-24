e244f1312075e4fe148216f8e9d18b37492608c1
Colin Contreary <colinccontreary@gmail.com>
2018-02-23 17:45:07 -0800

    Added subfetches of tracks to each playlist

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class HoursCounter extends Component {
 
     return (
       <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
-        <h2>{Math.round(totalDuration / 60)} hours</h2>
+        <h2>{Math.round(totalDuration / 3600)} hours</h2>
       </div>
     );
   }
@@ -89,11 +89,17 @@ class Filter extends Component {
 class Playlist extends Component {
   render() {
     let playlist = this.props.playlist;
-    return <div style={{ ...defaultStyle, display: 'inline-block', width: '25%' }}>
-        <img src={playlist.imageUrl ? playlist.imageUrl : require('./record.jpg')} style={{ width: '60px' }} alt=''/>
+    return (
+      <div style={{ ...defaultStyle, display: 'inline-block', width: '25%' }}>
+        <img
+          src={playlist.imageUrl ? playlist.imageUrl : require('./record.jpg')}
+          style={{ width: '60px' }}
+          alt=""
+        />
         <h3>{playlist.name}</h3>
         <ul>{playlist.songs.map(song => <li>{song.name}</li>)}</ul>
-      </div>;
+      </div>
+    );
   }
 }
 
@@ -113,7 +119,7 @@ class App extends Component {
     if (!accessToken) return;
 
     fetch('https://api.spotify.com/v1/me', {
-      headers: { 'Authorization': 'Bearer ' + accessToken }
+      headers: { Authorization: 'Bearer ' + accessToken }
     })
       .then(response => response.json())
       .then(data =>
@@ -122,37 +128,65 @@ class App extends Component {
             name: data.display_name
           }
         })
-      )
+      );
 
     fetch('https://api.spotify.com/v1/me/playlists', {
-      headers: { 'Authorization': 'Bearer ' + accessToken }
+      headers: { Authorization: 'Bearer ' + accessToken }
     })
       .then(response => response.json())
-      .then(data => {
-        this.setState({
-          playlists: data.items.map(item => {
-            console.log(data.items);
+      .then(playlistData => {
+        let playlists = playlistData.items
+
+        let trackDataPromises = playlists.map(playlist => {
+          let responsePromise = fetch(playlist.tracks.href, {
+            headers: { Authorization: 'Bearer ' + accessToken }
+          })
+          let trackDataPromise = responsePromise
+            .then(response => response.json())
+          return trackDataPromise
+      })
+
+      let allTracksDataPromises = 
+        Promise.all(trackDataPromises)
+
+      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+        trackDatas.forEach((trackData, i) => {
+          playlists[i].trackDatas = trackData.items
+            .map(item => item.track)
+            .map(trackData => ({
+              name: trackData.name,
+              duration: trackData.duration_ms / 1000
+            }))
+        })
+        return playlists
+      })
+
+      return playlistsPromise
+      })
+      .then(playlists => this.setState({
+          playlists: playlists.map(item => {
             return {
               name: item.name,
               imageUrl: item.images[0].url ? item.images[0].url : null,
-              songs: []
-            }})
+              songs: item.trackDatas.slice(0,3)
+            }
           })
-        })
+        }))
 
   }
 
   render() {
     let playlistsToRender =
-      this.state.user && 
-      this.state.playlists
+      this.state.user && this.state.playlists
         ? this.state.playlists.filter(playlist =>
-            playlist.name.toLowerCase().includes(
-              this.state.filterString.toLowerCase()))
-        : []
+            playlist.name
+              .toLowerCase()
+              .includes(this.state.filterString.toLowerCase())
+          )
+        : [];
     return (
       <div className="App">
-        {this.state.user ? 
+        {this.state.user ? (
           <div>
             <h1 style={{ ...defaultStyle, fontSize: '54px' }}>
               {this.state.user.name}'s Playlist
@@ -168,15 +202,18 @@ class App extends Component {
               <Playlist playlist={playlist} />
             ))}
           </div>
-         : <button onClick={() => {
+        ) : (
+          <button
+            onClick={() => {
               window.location = window.location.href.includes('localhost')
-              ? 'http://localhost:8888/login'
-              : 'https://better-playlists-test-backend.herokuapp.com/login'
-         }
-              }
+                ? 'http://localhost:8888/login'
+                : 'https://better-playlists-test-backend.herokuapp.com/login';
+            }}
             style={{ padding: '20px', fontSize: '50px', marginTop: '20px' }}
-          >Sign in with Spotify</button>
-        }
+          >
+            Sign in with Spotify
+          </button>
+        )}
       </div>
     );
   }
